1b9ad7313df8c679eaa5f31dfcfa352e760dcb7b
TH_97 <hood3392@gmail.com>
2024-03-19 17:54:09 +0900

    aocs

--- adminMainPage.js
+++ adminMainPage.js
@@ -4,35 +4,16 @@ $("#tree").treeview({
   animated: "fast"
   });
 
-  var floor = document.querySelector("#floor");
-  floor.addEventListener("click",() => {
-    console.log(1);
-    var main = document.querySelector("#main");
-    main.innerHTML = '<iframe src="1F.html" style="width: 100%; height: 100%; border:none"></iframe>';
-  });
+  var dayManage = document.querySelector("#dayManage")
 
-  var floor2 = document.querySelector("#floor2");
-  floor2.addEventListener("click",() => {
+  dayManage.addEventListener("click", ()=>{
     var main = document.querySelector("#main");
-    main.src = "2F.html";
+    main.src = "adminHtml/roomAssignment.html"
   });
+  var penaltyDetails = document.querySelector("#penaltyDetails")
+  penaltyDetails.addEventListener("click", ()=>{
+  var main = document.querySelector("#main");
+  main.src = "adminHtml/penaltyDetails.html"
+});
 
-  
-  
-  var penalt = document.querySelector("#penalt");
-  penalt.addEventListener("click",() =>{
-    var main = document.querySelector("#main");
-    main.innerHTML = '<iframe src="PenaltyDetails.html" style="width: 100%; height: 100%; border:none"></iframe>';
-  });
 
-  var suggestInquiry = document.querySelector("#suggestInquiry");
-  suggestInquiry.addEventListener("click",() =>{
-    var main = document.querySelector("#main");
-    main.innerHTML = '<iframe src="suggestInquiry.html" style="width: 100%; height: 100%; border:none"></iframe>';
-  });
-
-  var noticeInquiry = document.querySelector("#noticeInquiry");
-  noticeInquiry.addEventListener("click",() =>{
-    var main = document.querySelector("#main");
-    main.innerHTML = '<iframe src="noticeInquiry.html" style="width: 100%; height: 100%; border:none"></iframe>';
-  });
